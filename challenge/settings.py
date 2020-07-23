0aee47fbd1306a7dc5c93e537ec2f193be1d01e8
user <macbook_tb@MacBook-Pro-de-Macbook-tb.local>
2020-07-23 13:40:46 -0500

    Added my site to allowed hosts in Django

--- challenge/settings.py
+++ challenge/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'z%p$8rqbz-n#@r^$-78w=_bgjbl@*vc5p$-d_fvyp!raw%q)(4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['testing-dev.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
